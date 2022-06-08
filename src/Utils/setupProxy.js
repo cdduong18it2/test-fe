11b0d401083441961c23ac82996d382466c87997
cdduong18it2 <dangduongkt100@gmail.com>
2022-06-08 21:25:35 +0700

    hhh

--- src/Utils/setupProxy.js
+++ src/Utils/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 
 module.exports = app => {
 	app.use(
-		createProxyMiddleware("/api/v2/list_movies.json?", {
+		createProxyMiddleware("", {
 			target: "https://yts.torrentbay.to/"
 		}),
 	);
